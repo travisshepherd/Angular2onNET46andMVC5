34526dd75876f0f9561f73927fe02798e00810a8
Travis Shepherd <travisshepherd@outlook.com>
2016-08-31 19:39:46 -0500

    initial commit

---
+++ WebApplication1/src/app.component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Angular 2 running on .NET 4.6 and ASP.NET MVC 5</h1>'
+})
+export class AppComponent { }
